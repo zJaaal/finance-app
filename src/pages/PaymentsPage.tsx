c2eaeb132a4234097f50d83f436c1d2fd41e8eb0
Jalinson Diaz <zjaaaldev@gmail.com>
2022-07-26 19:21:26 -0300

    Added simple Markup

--- src/pages/PaymentsPage.tsx
+++ src/pages/PaymentsPage.tsx
@@ -1,49 +1,63 @@
+import { Grid, Typography, ButtonGroup, Button } from "@mui/material";
+import { GridRowsProp, GridColDef, DataGrid } from "@mui/x-data-grid";
 import React from "react";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import { Button, ButtonGroup } from "@mui/material";
-import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
-import ISaving from "../interfaces/ISaving";
+import IPayment from "../interfaces/IPayment";
 
-const SavingsPage = () => {
-  const rows: GridRowsProp<ISaving> = [
+const PaymentsPage = () => {
+  const rows: GridRowsProp<IPayment> = [
     {
       id: 1,
-      amount: 3000,
+      title: "Salary",
+      description: "",
+      amount: 100000,
       date: "26/07/2022",
     },
     {
       id: 2,
+      title: "Some payment I got from mentoring",
+      description: "",
       amount: 5000,
       date: "28/07/2022",
     },
     {
       id: 3,
-      amount: 9000,
+      title: "salary",
+      description: "",
+      amount: 100000,
       date: "29/07/2022",
     },
     {
       id: 4,
+      title: "Mentoring",
+      description: "",
       amount: 15000,
       date: "30/07/2022",
     },
     {
       id: 5,
+      title: "50% of payment",
+      description: "Diego's web page",
       amount: 50000,
       date: "01/08/2022",
     },
     {
       id: 6,
+      title: "Favor I did to Diego",
+      description: "",
       amount: 40000,
       date: "03/08/2022",
     },
     {
       id: 9,
+      title: "Mentoring payment",
+      description: "",
       amount: 6000,
       date: "05/08/2022",
     },
     {
       id: 10,
+      title: "30% of work",
+      description: "Alberto's page",
       amount: 50000,
       date: "15/08/2022",
     },
@@ -53,6 +67,11 @@ const SavingsPage = () => {
       field: "id",
       headerName: "ID",
       flex: 0.2,
+    },
+    {
+      field: "title",
+      headerName: "Title",
+      flex: 1,
     },
     {
       field: "amount",
@@ -69,7 +88,7 @@ const SavingsPage = () => {
     <Grid container item xs direction={"column"} height={"100%"}>
       <Grid item xs={1} padding={2}>
         <Typography variant="h4" color="initial" align="center">
-          Savings
+          Payments
         </Typography>
       </Grid>
       <Grid item xs={9} padding={2}>
@@ -83,18 +102,11 @@ const SavingsPage = () => {
         padding={2}
       >
         <Grid item>
-          <ButtonGroup
-            variant="contained"
-            color="primary"
-            aria-label="Savings Options"
-          >
-            <Button>Add Saving</Button>
-            <Button variant="outlined">Use Savings</Button>
-          </ButtonGroup>
+          <Button variant={"contained"}>Add Payment</Button>
         </Grid>
       </Grid>
     </Grid>
   );
 };
 
-export default SavingsPage;
+export default PaymentsPage;
