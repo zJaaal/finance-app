0f64057b1182a8698b73e8866c8e5eb0c7f35bb7
Jalinson Diaz <zjaaaldev@gmail.com>
2022-07-26 20:26:18 -0300

    Implemented StoreProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { CssBaseline, Paper } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router/AppRouter";
+import StoreProvider from "./store/StoreProvider";
 
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <CssBaseline />
-      <AppRouter />
+      <StoreProvider>
+        <CssBaseline />
+        <AppRouter />
+      </StoreProvider>
     </BrowserRouter>
   );
 }
