5fc3bc218775401c7e95f0650a47f5ed78ccd565
Jalinson Diaz <zjaaaldev@gmail.com>
2022-07-28 17:18:00 -0300

    Update SavingsPage.tsx

--- src/pages/SavingsPage.tsx
+++ src/pages/SavingsPage.tsx
@@ -1,53 +1,40 @@
-import React from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import { Button, ButtonGroup } from "@mui/material";
+import { Button } from "@mui/material";
+import { IRootState } from "../reducers/rootReducer";
+import { useDispatch, useSelector } from "react-redux";
 import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
+
+import SavingsModal from "../components/savings-page/SavingsModal";
+import ISavingState from "../reducers/saving/ISavingState";
 import ISaving from "../interfaces/ISaving";
+import { modalOpen } from "../actions/ui/modal";
+import {
+  cleanActiveSaving,
+  deleteSaving,
+  selectActiveSaving,
+} from "../actions/saving/saving";
 
 const SavingsPage = () => {
-  const rows: GridRowsProp<ISaving> = [
-    {
-      id: 1,
-      amount: 3000,
-      date: "26/07/2022",
-    },
-    {
-      id: 2,
-      amount: 5000,
-      date: "28/07/2022",
-    },
-    {
-      id: 3,
-      amount: 9000,
-      date: "29/07/2022",
-    },
-    {
-      id: 4,
-      amount: 15000,
-      date: "30/07/2022",
-    },
-    {
-      id: 5,
-      amount: 50000,
-      date: "01/08/2022",
-    },
-    {
-      id: 6,
-      amount: 40000,
-      date: "03/08/2022",
-    },
-    {
-      id: 9,
-      amount: 6000,
-      date: "05/08/2022",
-    },
-    {
-      id: 10,
-      amount: 50000,
-      date: "15/08/2022",
-    },
-  ];
+  const dispatch = useDispatch();
+  const saving: ISavingState = useSelector((state: IRootState) => state.saving);
+
+  const handleClick = () => {
+    dispatch(modalOpen());
+  };
+
+  const handleRowClick = () => {
+    dispatch(cleanActiveSaving());
+  };
+  const handleRowDoubleClick = ({ row }: { row: ISaving }) => {
+    dispatch(selectActiveSaving(row));
+  };
+
+  const handleDelete = () => {
+    dispatch(deleteSaving());
+  };
+
+  const rows: GridRowsProp<ISaving> = saving.savings;
   const columns: GridColDef[] = [
     {
       field: "id",
@@ -67,13 +54,20 @@ const SavingsPage = () => {
   ];
   return (
     <Grid container item xs direction={"column"} height={"100%"}>
+      <SavingsModal />
       <Grid item xs={1} padding={2}>
         <Typography variant="h4" color="initial" align="center">
           Savings
         </Typography>
       </Grid>
       <Grid item xs={8} padding={2}>
-        <DataGrid columns={columns} rows={rows} />
+        <DataGrid
+          columns={columns}
+          rows={rows}
+          onRowClick={handleRowClick}
+          onRowDoubleClick={handleRowDoubleClick}
+          disableSelectionOnClick
+        />
       </Grid>
       <Grid
         container
@@ -82,15 +76,20 @@ const SavingsPage = () => {
         xs={1}
         padding={2}
       >
-        <Grid item>
-          <ButtonGroup
-            variant="contained"
-            color="primary"
-            aria-label="Savings Options"
+        <Grid item marginRight={1}>
+          <Button
+            variant={"contained"}
+            onClick={handleDelete}
+            color="error"
+            disabled={!saving.activeSaving}
           >
-            <Button>Add Saving</Button>
-            <Button variant="outlined">Use Savings</Button>
-          </ButtonGroup>
+            Delete
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" onClick={handleClick}>
+            {!saving.activeSaving ? "Add Saving" : "Update Saving"}
+          </Button>
         </Grid>
       </Grid>
     </Grid>
