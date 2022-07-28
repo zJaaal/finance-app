43d64d905494a35238077e7558b1992a56a4ea9e
Jalinson Diaz <zjaaaldev@gmail.com>
2022-07-28 13:34:48 -0300

    Added types for earning action

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,6 +1,12 @@
 const types = {
   modalOpen: "[Modal] Modal Opened",
   modalClose: "[Modal] Modal Closed",
+
+  earningAdd: "[Earning] Earning added",
+  earningDelete: "[Earning] Earning deleted",
+  earningUpdate: "[Earning] Earning updated",
+  earningSelectActive: "[Earning] Earning active selected",
+  earningCleanActive: "[Earning] Earning active cleaned",
 };
 
 export default types;
