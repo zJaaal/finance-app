c72fb13ea40ec14cedd76056ef60d7d43c5b8b48
Jalinson Diaz <zjaaaldev@gmail.com>
2022-07-28 17:17:50 -0300

    Added saving to store

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import earningReducer from "./earning/earningReducer";
+import savingReducer from "./saving/savingReducer";
 import modalReducer from "./ui/modalReducer";
 
 const rootReducer = combineReducers({
   modal: modalReducer,
   earning: earningReducer,
+  saving: savingReducer,
 });
 
 export type IRootState = ReturnType<typeof rootReducer>;
