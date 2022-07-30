09c0e2f6a3e0541d3f12701e93e6128903da3370
Jalinson Diaz <zjaaaldev@gmail.com>
2022-07-30 18:51:52 -0300

    Implemented redux for Expenses

---
+++ src/reducers/expense/expenseReducer.ts
@@ -0,0 +1,70 @@
+import ExpenseActions from "../../actions/expense/enum/ExpenseActions";
+import { expenseActions } from "../../actions/expense/expense";
+import IExpense from "../../interfaces/IExpense";
+import IExpenseState from "./IExpenseState";
+
+const initialState: IExpenseState = {
+  expenses: [
+    {
+      id: 1,
+      title: "PS5",
+      description: "",
+      amount: 100000,
+      date: "07/26/2022",
+    },
+    {
+      id: 2,
+      title: "Jogger",
+      description: "",
+      amount: 5000,
+      date: "07/28/2022",
+    },
+  ],
+  activeExpense: null,
+};
+
+const expenseReducer = (
+  state: IExpenseState = initialState,
+  action: expenseActions
+) => {
+  switch (action.type) {
+    case ExpenseActions.EXPENSE_ADD: {
+      return {
+        ...state,
+        expenses: [action.payload, ...state.expenses],
+      };
+    }
+    case ExpenseActions.EXPENSE_SELECT_ACTIVE: {
+      return {
+        ...state,
+        activeExpense: action.payload,
+      };
+    }
+    case ExpenseActions.EXPENSE_CLEAN_ACTIVE: {
+      return {
+        ...state,
+        activeExpense: null,
+      };
+    }
+    case ExpenseActions.EXPENSE_UPDATE: {
+      return {
+        ...state,
+        expenses: state.expenses.map((expense) =>
+          expense.id == action.payload.id ? action.payload : expense
+        ),
+      };
+    }
+    case ExpenseActions.EXPENSE_DELETE: {
+      return {
+        ...state,
+        expenses: state.expenses.filter(
+          (expense) => expense.id != state.activeExpense?.id
+        ),
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default expenseReducer;
--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import earningReducer from "./earning/earningReducer";
+import expenseReducer from "./expense/expenseReducer";
 import savingReducer from "./saving/savingReducer";
 
 const rootReducer = combineReducers({
   earning: earningReducer,
   saving: savingReducer,
+  expense: expenseReducer,
 });
 
 export type IRootState = ReturnType<typeof rootReducer>;
