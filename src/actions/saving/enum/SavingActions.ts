71665bd59dcb68de64f7d809033ab98c2875cbb2
Jalinson Diaz <zjaaaldev@gmail.com>
2022-07-30 15:28:00 -0300

    Added types for saving actions

---
+++ src/actions/saving/enum/SavingActions.ts
@@ -0,0 +1,9 @@
+enum SavingActions {
+  savingAdd = "[Saving] Saving added",
+  savingDelete = "[Saving] Saving deleted",
+  savingUpdate = "[Saving] Saving updated",
+  savingSelectActive = "[Saving] Saving active selected",
+  savingCleanActive = "[Saving] Saving active cleaned",
+}
+
+export default SavingActions;
