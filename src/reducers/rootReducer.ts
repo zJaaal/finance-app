84fc412e836d9eb12a4f94836446d1e33bcc6bcb
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-01 19:13:30 -0300

    Added debtReducer to Redux Store

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+import debtReducer from "./debt/debtReducer";
 import earningReducer from "./earning/earningReducer";
 import expenseReducer from "./expense/expenseReducer";
 import savingReducer from "./saving/savingReducer";
@@ -7,6 +8,7 @@ const rootReducer = combineReducers({
   earning: earningReducer,
   saving: savingReducer,
   expense: expenseReducer,
+  debt: debtReducer,
 });
 
 export type IRootState = ReturnType<typeof rootReducer>;
