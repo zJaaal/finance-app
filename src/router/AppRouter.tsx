0f3f2f112d54598789e6b0a2d457df8ff7f734dd
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-02 18:07:55 -0300

    Added documentation and fixed some responsive errors

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -9,19 +9,27 @@ import DebtsPage from "../pages/DebtsPage";
 import SavingsPage from "../pages/SavingsPage";
 import SettingsPage from "../pages/SettingsPage";
 import EarningsPage from "../pages/EarningsPage";
+import PaymentsPage from "../pages/PaymentsPage";
 
 const AppRouter = () => {
   return (
     <Grid container direction={"column"} height={"inherit"}>
-      <NavBar />
+      {/* This bar should be online on Private Routes */}
+      <Grid item width={"100%"} xs={1}>
+        <NavBar />
+      </Grid>
       <Routes>
+        {/* Private Routes */}
         <Route path="/" element={<DashboardPage />} />
         <Route path="/earnings" element={<EarningsPage />} />
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/expenses" element={<ExpensesPage />} />
         <Route path="/debts" element={<DebtsPage />} />
         <Route path="/savings" element={<SavingsPage />} />
         <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/payments/:debtId" element={<PaymentsPage />} />
+
+        {/* Public Routes */}
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Grid>
   );
