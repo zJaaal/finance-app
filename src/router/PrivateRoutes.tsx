40e9213d58671eaa35f927d0a987cbf72f1e6dbd
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-03 20:35:32 -0300

    Added Route Protection

---
+++ src/router/PrivateRoutes.tsx
@@ -0,0 +1,25 @@
+import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Navigate, useNavigate } from "react-router-dom";
+import { IRootState } from "../reducers/rootReducer";
+
+const PrivateRoutes = ({
+  children,
+}: {
+  children: ReactJSXElement;
+}): ReactJSXElement => {
+  const { uid }: { uid: number | null } = useSelector(
+    (state: IRootState) => state.auth
+  );
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!uid) {
+      navigate("/login");
+    }
+  }, [uid]);
+  return children;
+};
+
+export default PrivateRoutes;
---
+++ src/router/PublicRoutes.tsx
@@ -0,0 +1,27 @@
+import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
+import React, { ReactNode, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Navigate, useNavigate } from "react-router-dom";
+import { IRootState } from "../reducers/rootReducer";
+
+const PublicRoutes = ({
+  children,
+}: {
+  children: ReactJSXElement;
+}): ReactJSXElement => {
+  const { uid }: { uid: number | null } = useSelector(
+    (state: IRootState) => state.auth
+  );
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (uid) {
+      navigate("/");
+    }
+  }, [uid]);
+
+  return children;
+};
+
+export default PublicRoutes;
