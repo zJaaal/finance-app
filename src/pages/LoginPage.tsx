84e41ed89ef3ea15f34cd27a2537094b4af876b0
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-03 17:11:35 -0300

    Added simple markup for login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,40 @@
+import { Grid, Paper, Typography } from "@mui/material";
 import React from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <Grid
+      container
+      justifyContent={"center"}
+      alignItems={"center"}
+      height="inherit"
+    >
+      <Grid
+        item
+        xs={11}
+        sm={6}
+        md={6}
+        xl={6}
+        container
+        height={"70%"}
+        direction={"column"}
+        alignItems={"center"}
+      >
+        <Paper
+          elevation={4}
+          sx={{ width: "100%", padding: "10px", height: "100%" }}
+        >
+          <Grid container direction={"column"} height={"inherit"}>
+            <Grid item>
+              <Typography variant="h6" color="initial" align="center">
+                Finance App WIP
+              </Typography>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default LoginPage;
